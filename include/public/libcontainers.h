ebc836eeaf5df68a0f21921e6014b287ea979ca4
MaxenceROBIN <maxence.robin3@gmail.com>
2021-12-16 23:59:37 +0100

    MOD: Refactor of container algorithms

--- include/public/libcontainers.h
+++ include/public/libcontainers.h
@@ -16,13 +16,15 @@ struct container;
 
 /* API -----------------------------------------------------------------------*/
 
+/* Callback implementations ----------*/
+
 /**
  * @brief Returns an iterator on the first element of 'ctx'.
  *
  * @return Pointer to the first element on success.
  * @return NULL if 'ctx' is invalid or does not support first iterator access.
  */
-struct iterator *container_first(const struct container *ctx);
+struct iterator *ctn_first(const struct container *ctx);
 
 /**
  * @brief Returns an iterator on the last element of 'ctx'.
@@ -30,7 +32,7 @@ struct iterator *container_first(const struct container *ctx);
  * @return Pointer to the last element on success.
  * @return NULL if 'ctx' is invalid or does not support last iterator access.
  */
-struct iterator *container_last(const struct container *ctx);
+struct iterator *ctn_last(const struct container *ctx);
 
 /**
  * @brief Inserts 'data' inside 'ctx' where 'it' points to.
@@ -40,8 +42,7 @@ struct iterator *container_last(const struct container *ctx);
  * @return -EINVAL if 'ctx', 'it' or 'data' are invalid.
  * @return A negative errno for other failures.
  */
-int container_insert(
-                struct container *ctx, struct iterator *it, const void *data);
+int ctn_insert_it(struct container *ctx, struct iterator *it, const void *data);
 
 /**
  * @brief Removes the data pointed by 'it' inside 'ctx'.
@@ -51,7 +52,9 @@ int container_insert(
  * @return -EINVAL if 'ctx' or 'it' are invalid.
  * @return A negative errno for other failures.
  */
-int container_remove(struct container *ctx, const struct iterator *it);
+int ctn_remove_it(struct container *ctx, const struct iterator *it);
+
+/* Algorithms ------------------------*/
 
 /**
  * @brief Calls 'cb' for each element of 'ctx', with 'data' as context.
@@ -66,29 +69,145 @@ int container_remove(struct container *ctx, const struct iterator *it);
  * @return A negative errno if the iteration failed.
  * @return A custom negative errno if a call to 'cb' failed.
  */
-int container_for_each(
+int ctn_for_each(
                 struct container *ctx,
                 int (*cb)(struct iterator *, void *),
                 void *data);
 
 /**
- * @brief Calls 'cb' for each element of 'ctx' in reverse order, with 'data' as
- * context.
+ * @brief Returns the number of elements matching 'data' inside 'ctx'.
+ *
+ * @return The number of matching elements on success.
+ * @return -EINVAL if 'ctx' or 'data' are invalid.
+ * @return A negative errno on failure.
+ */
+int ctn_count(const struct container *ctx, const void *data);
+
+/**
+ * @brief Returns the number of elements matching 'cb' inside 'ctx'.
  *
  * @param cb : For each call, the element of 'ctx' will be the first argument
- * and 'data' will be the second. It must return 0 on success and a negative
- * errno on failure.
+ * and 'data' will be the second. It must return 'true' if the element matches
+ * the criteria and 'false' otherwise.
+ *
+ * @return The number of matchin elements on success.
+ * @return -EINVAL if 'ctx' or 'cb' are invalid.
+ * @return A negative errno on failure.
+ */
+int ctn_count_if(
+                const struct container *ctx,
+                bool (*cb)(const void *, void *),
+                void *data);
+
+/**
+ * @brief Finds 'data' inside 'ctx'.
+ *
+ * @return An iterator over the found data on success.
+ * @return NULL on failure.
+ */
+struct iterator *ctn_find(const struct container *ctx, const void *data);
+
+/**
+ * @brief Finds an element matching 'cb' inside 'ctx'.
+ *
+ * @param cb : For each call, the element of 'ctx' will be the first argument
+ * and 'data' will be the second. It must return 'true' if the element matches
+ * the criteria and 'false' otherwise.
+ *
+ * @return An iterator over the found data on success.
+ * @return NULL on failure.
+ */
+struct iterator *ctn_find_if(
+                const struct container *ctx,
+                bool (*cb)(const void *, void *),
+                void *data);
+
+/**
+ * @brief Indicates if 'ctx' contains 'data'.
+ *
+ * @return true if 'ctx' contains 'data'.
+ * @return false if 'ctx' does not contain 'data', if 'ctx' or 'data' are
+ * invalid or if the iteration failed.
+ */
+bool ctn_contains(const struct container *ctx, const void *data);
+
+/**
+ * @brief Indicates if any of the elements of 'ctx' are matching 'cb'.
+ *
+ * @param cb : For each call, the element of 'ctx' will be the first argument
+ * and 'data' will be the second. It must return 'true' if the element matches
+ * the criteria and 'false' otherwise.
+ *
+ * @return true if any element of 'ctx' matches the criteria.
+ * @return false if no element matches, if 'ctx' or 'cb' are invalid or if the
+ * iteration failed.
+ */
+bool ctn_any_of(
+                const struct container *ctx,
+                bool (*cb)(const void *, void *),
+                void *data);
+
+/**
+ * @brief Indicates if all the elements of 'ctx' are matchin 'cb'.
+ *
+ * @param cb : For each call, the element of 'ctx' will be the first argument
+ * and 'data' will be the second. It must return 'true' if the element matches
+ * the criteria and 'false' otherwise.
+ *
+ * @return true if all the elements of 'ctx' are matching the criteria.
+ * @return false if any element does not match, if 'ctx' or 'cb' are invalid or
+ * if the iteration failed.
+ */
+bool ctn_all_of(
+                const struct container *ctx,
+                bool (*cb)(const void *, void *),
+                void *data);
+
+/**
+ * @brief Fills 'ctx' with 'data'.
  *
  * @return 0 on success.
- * @return -EINVAL if 'ctx' or 'cb' are invalid.
- * @return -ENOMEM if the last element failed.
- * @return A negative errno if the iteration failed.
- * @return A custom negative errno if a call to 'cb' failed.
+ * @return -EINVAL if 'ctx' or 'data' are invalid.
+ * @return A negative errno on failure.
  */
-int container_for_each_r(
-                struct container *ctx,
-                int (*cb)(struct iterator *, void *),
-                void *data);
+int ctn_fill(struct container *ctx, const void *data);
+
+/**
+ * @brief Fills 'ctx' with data provided by 'cb'.
+ *
+ * @param cb : For each call, data' will be passed. It must return a value to be
+ * inserted inside 'ctx'.
+ *
+ * @return 0 on success.
+ * @return -EINVAL if 'ctx' or 'data' are invalid.
+ * @return A negative errno on failure.
+ */
+int ctn_generate(struct container *ctx, const void *(*cb)(void *), void *data);
+
+/**
+ * @brief Returns the minimum value inside 'ctx'.
+ *
+ * @return Pointer to the minimum value on success.
+ * @return NULL if 'ctx' is invalid or on failure.
+ */
+const void *ctn_min(const struct container *ctx);
+
+/**
+ * @brief Returns the maximum value inside 'ctx'.
+ *
+ * @return Pointer to the maximum value on success.
+ * @return NULL if 'ctx' is invalid or on failure.
+ */
+const void *ctn_max(const struct container *ctx);
+
+/**
+ * @brief Removes the first element matching 'data' inside 'ctx'.
+ *
+ * @return 0 on success.
+ * @return -EINVAL if 'ctx' or 'data' are invalid.
+ * @return A negative errno on failure.
+ */
+int ctn_remove(struct container *ctx, const void *data);
 
 /**
  * @brief Filters elements of 'ctx' based on 'cb', with 'data' as context.
@@ -102,26 +221,9 @@ int container_for_each_r(
  * @return -ENOMEM if the first element failed.
  * @return A negative errno if the iteration failed.
  */
-int container_remove_if(
+int ctn_remove_if(
                 struct container *ctx,
                 bool (*cb)(const void *, void *),
                 void *data);
-
-/**
- * @brief Finds 'data' inside 'ctx'.
- *
- * @return An iterator over the found data on success.
- * @return NULL on failure.
- */
-struct iterator *container_find(const struct container *ctx, const void *data);
-
-/**
- * @brief Indicates if 'ctx' contains 'data'.
- *
- * @return true if 'ctx' contains 'data'.
- * @return false if 'ctx' does not contain 'data', if 'ctx' or 'data' are
- * invalid or if the iteration failed.
- */
-bool container_contains(const struct container *ctx, const void *data);
 
 #endif /* LIB_CONTAINERS_H */
--- src/libcontainers.c
+++ src/libcontainers.c
@@ -12,79 +12,52 @@
 #include <stddef.h>
 #include <string.h>
 
+/* Definitions ---------------------------------------------------------------*/
+
+enum loop_action {
+        LOOP_ACTION_CONTINUE = 1, /* Don't interfer with 0 return value */
+        LOOP_ACTION_REMOVE,
+        LOOP_ACTION_STOP
+};
+
 /* Static functions ----------------------------------------------------------*/
 
-/**
- * @brief Calls 'cb' with 'data' for each element of 'ctx', starting with the
- * element given by 'it_get' and iterating using 'it_iterate'.
- *
- * @return 0 on success.
- * @return -ENOMEM if 'it_get' failed.
- * @return A negative errno if the iteration failed.
- * @return A custom negative errno if a call to 'cb' failed.
- *
- * @note For each call to 'cb', the element of 'ctx' will be the first argument
- * and 'data' will be the second.
- * @warning 'cb' MUST return a negative errno on failure, and 0 on success.
- */
-static int for_each(
+static int loop(
                 struct container *ctx,
-                int (*cb)(struct iterator *, void *),
-                void *data,
-                struct iterator *(*it_get)(const struct container *),
-                int (*it_iterate)(struct iterator *))
+                int (*cb)(struct container *, struct iterator *, void *),
+                void *args)
 {
         int res;
-        struct iterator *it = it_get(ctx);
+
+        struct iterator *it = ctn_first(ctx);
         if (!it)
                 return -ENOMEM;
 
         while (iterator_is_valid(it)) {
-                res = cb(it, data);
+                res = cb(ctx, it, args);
                 if (res < 0)
-                        goto error_call;
+                        goto error_iterate;
 
-                res = it_iterate(it);
+                if (res == LOOP_ACTION_STOP)
+                        return LOOP_ACTION_STOP;
+
+                res = (res == LOOP_ACTION_REMOVE) ?
+                                ctn_remove_it(ctx, it) : iterator_next(it);
                 if (res < 0)
                         goto error_iterate;
         }
 
         res = 0;
 error_iterate:
-error_call:
         iterator_destroy(it);
         return res;
 }
 
-/**
- * @brief Finds 'data' inside 'ctx'.
- *
- * @return An iterator over the found data on success.
- * @return NULL on failure.
- */
-static struct iterator *find_element(
-                const struct container *ctx, const void *data)
-{
-        struct iterator *it = container_first(ctx);
-        if (!it)
-                return NULL;
-
-        while (iterator_is_valid(it)) {
-                if (ctx->type->comp(
-                                iterator_data(it), data, ctx->type->size) == 0)
-                        return it;
-
-                if (iterator_next(it) < 0)
-                        break;
-        }
-
-        iterator_destroy(it);
-        return NULL;
-}
-
 /* API -----------------------------------------------------------------------*/
 
-struct iterator *container_first(const struct container *ctx)
+/* Callback implementations ----------*/
+
+struct iterator *ctn_first(const struct container *ctx)
 {
         if (!ctx || !ctx->cbs || !ctx->cbs->first)
                 return NULL;
@@ -92,7 +65,7 @@ struct iterator *container_first(const struct container *ctx)
         return ctx->cbs->first(ctx);
 }
 
-struct iterator *container_last(const struct container *ctx)
+struct iterator *ctn_last(const struct container *ctx)
 {
         if (!ctx || !ctx->cbs || !ctx->cbs->last)
                 return NULL;
@@ -100,8 +73,7 @@ struct iterator *container_last(const struct container *ctx)
         return ctx->cbs->last(ctx);
 }
 
-int container_insert(
-                struct container *ctx, struct iterator *it, const void *data)
+int ctn_insert_it(struct container *ctx, struct iterator *it, const void *data)
 {
         if (!ctx || !it || !data)
                 return -EINVAL;
@@ -112,7 +84,7 @@ int container_insert(
         return ctx->cbs->insert(ctx, it, data);
 }
 
-int container_remove(struct container *ctx, const struct iterator *it)
+int ctn_remove_it(struct container *ctx, const struct iterator *it)
 {
         if (!ctx || !it)
                 return -EINVAL;
@@ -123,7 +95,24 @@ int container_remove(struct container *ctx, const struct iterator *it)
         return ctx->cbs->remove(ctx, it);
 }
 
-int container_for_each(
+/* Algorithms ------------------------*/
+
+/* for_each ----------------*/
+
+struct for_each_args {
+        int (*cb)(struct iterator *, void *);
+        void *data;
+};
+
+static int for_each_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct for_each_args *args = data;
+
+        const int res = args->cb(it, args->data);
+        return (res < 0) ?  res : LOOP_ACTION_CONTINUE;
+}
+
+int ctn_for_each(
                 struct container *ctx,
                 int (*cb)(struct iterator *, void *),
                 void *data)
@@ -131,21 +120,329 @@ int container_for_each(
         if (!ctx || !cb)
                 return -EINVAL;
 
-        return for_each(ctx, cb, data, container_first, iterator_next);
+        struct for_each_args args = { .cb = cb, .data = data };
+        return loop(ctx, for_each_cb, &args);
 }
 
-int container_for_each_r(
-                struct container *ctx,
-                int (*cb)(struct iterator *, void *),
+/* count -------------------*/
+
+struct count_args {
+        const void *data;
+        int count;
+};
+
+static int count_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct count_args *args = data;
+
+        if (ctx->type->comp(
+                        args->data, iterator_data(it), ctx->type->size) == 0)
+                ++args->count;
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+int ctn_count(const struct container *ctx, const void *data)
+{
+        if (!ctx || !data)
+                return -EINVAL;
+
+        struct count_args args = { .data = data, .count = 0};
+        const int res = loop(ctx, count_cb, &args);
+        if (res < 0)
+                return res;
+
+        return args.count;
+}
+
+struct count_if_args {
+        bool (*cb)(const void *, void *);
+        void *data;
+        int count;
+};
+
+static int count_if_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct count_if_args *args = data;
+
+        if (args->cb(iterator_data(it), args->data))
+                ++args->count;
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+int ctn_count_if(
+                const struct container *ctx,
+                bool (*cb)(const void *, void *),
+                void *data)
+{
+        if (!ctx || !data)
+                return -EINVAL;
+
+        struct count_if_args args = { .cb = cb, .data = data, .count = 0};
+        const int res = loop(ctx, count_if_cb, &args);
+        if (res < 0)
+                return res;
+
+        return args.count;
+}
+
+/* find --------------------*/
+
+struct find_args {
+        const void *data;
+        struct iterator *it;
+};
+
+static int find_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct find_args *args = data;
+
+        if (ctx->type->comp(
+                        args->data, iterator_data(it), ctx->type->size) == 0) {
+                args->it = it;
+                return LOOP_ACTION_STOP;
+        }
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+struct iterator *ctn_find(const struct container *ctx, const void *data)
+{
+        if (!ctx || !data)
+                return -EINVAL;
+
+        struct find_args args = { .data = data };
+        if (loop(ctx, find_cb, &args) < 0)
+                return NULL;
+
+        return args.it;
+}
+
+struct find_if_args {
+        bool (*cb)(const void *, void *);
+        void *data;
+        struct iterator *it;
+};
+
+static int find_if_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct find_if_args *args = data;
+
+        if (args->cb(iterator_data(it), args->data)) {
+                args->it = it;
+                return LOOP_ACTION_STOP;
+        }
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+struct iterator *ctn_find_if(
+                const struct container *ctx,
+                bool (*cb)(const void *, void *),
                 void *data)
 {
         if (!ctx || !cb)
                 return -EINVAL;
 
-        return for_each(ctx, cb, data, container_last, iterator_previous);
+        struct find_if_args args = { .cb = cb, .data = data };
+        if (loop(ctx, find_if_cb, &args) < 0)
+                return NULL;
+
+        return args.it;
 }
 
-int container_remove_if(
+/* contains ----------------*/
+
+bool ctn_contains(const struct container *ctx, const void *data)
+{
+        if (!ctx || !data)
+                return -EINVAL;
+
+        struct find_args args = { .data = data };
+        if (loop(ctx, find_cb, &args) < 0)
+                return false;
+
+        iterator_destroy(args.it);
+        return true;
+}
+
+bool ctn_any_of(
+                const struct container *ctx,
+                bool (*cb)(const void *, void *),
+                void *data)
+{
+        if (!ctx || !cb)
+                return -EINVAL;
+
+        struct find_if_args args = { .cb = cb, .data = data };
+        if (loop(ctx, find_if_cb, &args) < 0)
+                return false;
+
+        iterator_destroy(args.it);
+        return true;
+}
+
+static int all_of_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct find_if_args *args = data;
+
+        if (!args->cb(iterator_data(it), args->data))
+                return -1;
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+bool ctn_all_of(
+                const struct container *ctx,
+                bool (*cb)(const void *, void *),
+                void *data)
+{
+        if (!ctx || !cb)
+                return -EINVAL;
+
+        struct find_if_args args = { .cb = cb, .data = data };
+        if (loop(ctx, all_of_cb, &args) < 0)
+                return false;
+
+        return true;
+}
+
+/* fill --------------------*/
+
+static int fill_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        const int res = iterator_set_data(it, data);
+        if (res < 0)
+                return res;
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+int ctn_fill(struct container *ctx, const void *data)
+{
+        if (!ctx || !data)
+                return -EINVAL;
+
+        return loop(ctx, fill_cb, (void *)data);
+}
+
+struct generate_args {
+        const void *(*cb)(void *);
+        void *data;
+};
+
+static int generate_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct generate_args *args = data;
+
+        const int res = iterator_set_data(it, args->cb(args->data));
+        if (res < 0)
+                return res;
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+int ctn_generate(struct container *ctx, const void *(*cb)(void *), void *data)
+{
+        if (!ctx || !cb)
+                return -EINVAL;
+
+        struct generate_args args = { .cb = cb, .data = data };
+        return loop(ctx, generate_cb, &args);
+}
+
+/* min/max -----------------*/
+
+enum min_max_mode {
+        MODE_MIN,
+        MODE_MAX
+};
+
+struct min_max_args {
+        const void *value;
+        enum min_max_mode mode;
+        bool first;
+};
+
+static int min_max_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct min_max_args *args = data;
+        const void *it_data = iterator_data(it);
+
+        if (args->first) {
+                args->value = it_data;
+                args->first = false;
+                return LOOP_ACTION_CONTINUE;
+        }
+
+        const int res = ctx->type->comp(it_data, args->value, ctx->type->size);
+        if (res < 0 && args->mode == MODE_MIN ||
+                        res > 0 && args->mode == MODE_MAX)
+                args->value = it_data;
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+static const void *min_max(const struct container *ctx, enum min_max_mode mode)
+{
+        struct min_max_args args = {
+                .mode = mode,
+                .first = true,
+                .value = NULL
+        };
+
+        const int res = loop(ctx, min_max_cb, &args);
+        if (res < 0)
+                return NULL;
+
+        return args.value;
+}
+
+const void *ctn_min(const struct container *ctx)
+{
+        if (!ctx)
+                return NULL;
+
+        return min_max(ctx, MODE_MIN);
+}
+
+const void *ctn_max(const struct container *ctx)
+{
+        if (!ctx)
+                return NULL;
+
+        return min_max(ctx, MODE_MAX);
+}
+
+/* remove ------------------*/
+
+int ctn_remove(struct container *ctx, const void *data)
+{
+        if (!ctx || !data)
+                return -EINVAL;
+
+        struct find_args args = { .data = data };
+        int res = loop(ctx, find_cb, &args);
+        if (res < 0)
+                return res;
+
+        res = ctn_remove_it(ctx, args.it);
+        iterator_destroy(args.it);
+        return res;
+}
+
+static int remove_if_cb(struct container *ctx, struct iterator *it, void *data)
+{
+        struct find_if_args *args = data;
+
+        if (args->cb(iterator_data(it), args->data))
+                return LOOP_ACTION_REMOVE;
+
+        return LOOP_ACTION_CONTINUE;
+}
+
+int ctn_remove_if(
                 struct container *ctx,
                 bool (*cb)(const void *, void *),
                 void *data)
@@ -153,42 +450,11 @@ int container_remove_if(
         if (!ctx || !cb)
                 return -EINVAL;
 
-        int res;
-        struct iterator *it = container_first(ctx);
-        if (!it)
-                return -ENOMEM;
+        struct find_if_args args = { .cb = cb, .data = data };
+        const int res = loop(ctx, remove_if_cb, &args);
+        if (res < 0)
+                return res;
 
-        while (iterator_is_valid(it)) {
-                res = (cb(iterator_data(it), data) ?
-                                container_remove(ctx, it) : iterator_next(it));
-
-                if (res < 0)
-                        goto error_iterate;
-        }
-
-        res = 0;
-error_iterate:
-        iterator_destroy(it);
-        return res;
-}
-
-struct iterator *container_find(const struct container *ctx, const void *data)
-{
-        if (!ctx || !data)
-                return NULL;
-
-        return find_element(ctx, data);
-}
-
-bool container_contains(const struct container *ctx, const void *data)
-{
-        if (!ctx || !data)
-                return false;
-
-        const struct iterator *it = find_element(ctx, data);
-        if (!it)
-                return false;
-
-        iterator_destroy(it);
-        return true;
+        iterator_destroy(args.it);
+        return 0;
 }
