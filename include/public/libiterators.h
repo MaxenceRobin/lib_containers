d3b3b3565d7f50c657d17174dd86541f05039d17
MaxenceROBIN <maxence.robin3@gmail.com>
2022-04-19 22:01:31 +0200

    ADD: Iterator API

---
+++ include/public/libiterators.h
@@ -0,0 +1,68 @@
+/**
+ * @author Maxence ROBIN
+ * @brief Provides simple iterators manipulation.
+ */
+
+#ifndef LIB_ITERATORS_H
+#define LIB_ITERATORS_H
+
+/* Includes ------------------------------------------------------------------*/
+
+#include <stdbool.h>
+
+/* Definition ----------------------------------------------------------------*/
+
+struct iterator;
+
+/* API -----------------------------------------------------------------------*/
+
+/**
+ * @brief Increases the reference count of 'it'.
+ *
+ * @return 'it' itself for convenience.
+ * @return NULL if 'it' is invalid.
+ */
+struct iterator *it_ref(struct iterator *it);
+
+/**
+ * @brief Decreses the reference count of 'it'. If the reference count reaches 0
+ * 'it' will be destroyed.
+ *
+ * @return 'it' itself for convenience if it's not destroyed.
+ * @return NULL if 'it' gets destroyed during the call or if 'it' is invalid.
+ */
+struct iterator *it_unref(struct iterator *it);
+
+/**
+ * @brief Change the value pointed by 'it' to the next one.
+ *
+ * @return 0 on success.
+ * @return -EINVAL if 'it' is invalid.
+ */
+int it_next(struct iterator *it);
+
+/**
+ * @brief Change the value pointed by 'it' to the previous one.
+ *
+ * @return 0 on success.
+ * @return -EINVAL if 'it' is invalid.
+ */
+int it_previous(struct iterator *it);
+
+/**
+ * @brief Indicates if 'it' points to a valid value.
+ *
+ * @return true if 'it' points to a valid value.
+ * @return -false if 'it' points to an invalid value or if 'it' is invalid.
+ */
+bool it_is_valid(const struct iterator *it);
+
+/**
+ * @brief Returns the value pointed by 'it'.
+ *
+ * @return The value pointed by 'it' on success.
+ * @return NULL if 'it' is invalid.
+ */
+void *it_data(const struct iterator *it);
+
+#endif /* LIB_ITERATORS_H */
